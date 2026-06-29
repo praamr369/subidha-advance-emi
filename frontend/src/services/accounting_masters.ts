16b4683ff74c333935002d03006e521b44ad10c8
Pradip Roy <proy7843@gmail.com>
2026-06-29 16:39:27 +0530

    refactor(frontend): add accounting masters service facade

---
+++ frontend/src/services/accounting_masters.ts
@@ -0,0 +1,21 @@
+export {
+  listChartOfAccounts,
+  createChartOfAccount,
+  getChartOfAccount,
+  updateChartOfAccount,
+  getChartOfAccountEditability,
+  listFinanceAccounts,
+  createFinanceAccount,
+  getFinanceAccount,
+  updateFinanceAccount,
+  getFinanceAccountEditability,
+} from "./accounting";
+
+export type {
+  AccountingPaginatedResponse,
+  ChartOfAccount,
+  ChartOfAccountDetail,
+  FinanceAccount,
+  FinanceAccountDetail,
+  AccountingEditabilityEnvelope,
+} from "./accounting";
